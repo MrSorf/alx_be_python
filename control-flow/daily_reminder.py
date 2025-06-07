948373e159be9859bbd678d606107bca0c34e660
MrSorf <your_email>
2025-06-07 19:40:09 +0100

    daily reminder edited

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -5,15 +5,14 @@ Priority = input("Enter task priority(high, medium, low): ")
 time_bound = input("Is the task time-bound(yes or no): ")
 
 match Priority: 
-    case high:
-          case "high":
-        reminder = f"'{task}' is a high priority task"
+    case "high":
+        reminder = f"'{Task}' is a high priority task"
     case "medium":
-        reminder = f"'{task}' is a medium priority task"
+        reminder = f"'{Task}' is a medium priority task"
     case "low":
-        reminder = f"'{task}' is a low priority task"
+        reminder = f"'{Task}' is a low priority task"
     case _:
-        reminder = f"'{task}' has an unknown priority level"
+        reminder = f"'{Task}' has an unknown priority level"
 
 # Modify reminder if time-bound
 if time_bound == "yes":
