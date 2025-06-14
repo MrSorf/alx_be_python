13ec9159763fbd720a04dbcb101b13e0369a768f
MrSorf <your_email>
2025-06-14 16:35:38 +0100

    files edited and updated

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,5 +1,5 @@
 class BankAccount:
-    def __init__(self, account_balance, balance=0):
+    def __init__(self, balance=0):
         self.account_balance = balance
     
     def deposit(self, amount):
