d45b8deb49c5b27d8210b6e71a5742545000ae4c
MrSorf <your_email>
2025-06-15 17:13:57 +0100

    punctuation error corrected(.)

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -7,9 +7,9 @@ def safe_divide(numerator, denominator):
       return f"The result of the division is {result}"
 
     except ValueError:
-       return "Error: Please enter numeric values only"
+       return "Error: Please enter numeric values only."
 
     except ZeroDivisionError:
-        return "Error: Cannot divide by zero"
+        return "Error: Cannot divide by zero."
     
 
