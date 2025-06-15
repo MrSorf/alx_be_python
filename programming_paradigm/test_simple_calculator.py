59654e2d061173d85c1050a633280b82f7989c06
MrSorf <your_email>
2025-06-15 18:46:05 +0100

    test file updated

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -8,13 +8,13 @@ class TestCalculator(unittest.TestCase):
     def test_addition(self):
         self.assertEqual(self.calc.add(2, 1), 3)
     
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(2, 1), 1)
     
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(2, 2), 4)
 
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(4, 2), 2)
         self.assertEqual(self.calc.divide(0, 4), 0)
         self.assertIsNone(self.calc.divide(4, 0))
